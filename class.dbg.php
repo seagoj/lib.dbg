1c6f0f08fd3b126cab26377af34cd3dde8cba6da
Jeremy Seago <seagoj@gmail.com>
2012-02-05 12:39:48 -0600

    Added assert() to class.dbg

--- class.dbg.php
+++ class.dbg.php
@@ -1,5 +1,4 @@
 <?php
-print "<div>Start class DBG ".rand()."</div>";
 /**
  * Description of class
  *
@@ -9,7 +8,7 @@ class dbg
 {
     function __construct()
     {}
-    public function msg($message, $method='', $file='', $line='')
+    public function msg($message, $method='', $exception=false, $file='', $line='')
     {
         print "<div class='err'>";
         $method=='' ? print '' : print "<span style='color:red;'>$method</span>: ";
@@ -17,6 +16,7 @@ class dbg
         $file=='' ? print '' : print "in file $file";
         $line=='' ? print '' : print "on line $line";
         print "</span></div>";
+        if($exception) throw Exception ($msg);
     }
     public function vardump($var, $label='')
     {
@@ -24,6 +24,15 @@ class dbg
         print "<div class='err'>";
         $label=='' ? print '' : print "<span style='color:red;'>$label</span>: ";
         print "<span style='color:black;'>$dump</span></div>";
+    }
+    public function assert($term) {
+        assert_options(ASSERT_ACTIVE, true);
+        assert_options(ASSERT_WARNING, true);
+        assert_options(ASSERT_BAIL, false);
+        assert_options(ASSERT_QUIET_EVAL, false);
+        assert_callback(ASSERT_CALLBACK, $this->msg($message,'', $script, $line));
+
+        assert($term);
     }
 }
 ?>
