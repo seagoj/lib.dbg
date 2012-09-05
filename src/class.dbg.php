6f75c035e722dba62fb69085ea524354c2375ca6
Jeremy Seago <seagoj@gmail.com>
2012-09-05 16:26:26 -0500

    Initial commit of static branch

--- src/class.dbg.php
+++ src/class.dbg.php
@@ -1,4 +1,9 @@
 <?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 /**
  * Description of class
  *
@@ -8,7 +13,8 @@ class dbg
 {
     function __construct()
     {}
-    public function msg($message, $method='', $exception=false, $file='', $line='')
+
+    public function msg($message, $method='', $file='', $line='')
     {
         print "<div class='err'>";
         $method=='' ? print '' : print "<span style='color:red;'>$method</span>: ";
@@ -16,7 +22,6 @@ class dbg
         $file=='' ? print '' : print "in file $file";
         $line=='' ? print '' : print "on line $line";
         print "</span></div>";
-        if($exception) throw Exception ($msg);
     }
     public function vardump($var, $label='')
     {
@@ -24,15 +29,6 @@ class dbg
         print "<div class='err'>";
         $label=='' ? print '' : print "<span style='color:red;'>$label</span>: ";
         print "<span style='color:black;'>$dump</span></div>";
-    }
-    public function assert($term) {
-        assert_options(ASSERT_ACTIVE, true);
-        assert_options(ASSERT_WARNING, true);
-        assert_options(ASSERT_BAIL, false);
-        assert_options(ASSERT_QUIET_EVAL, false);
-        assert_callback(ASSERT_CALLBACK, $this->msg($message,'', $script, $line));
-
-        assert($term);
     }
 }
 ?>
